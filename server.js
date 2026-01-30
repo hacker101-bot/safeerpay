ad2953e0e78ac6c49cc2f11ad7cb4f2825db4833
unknown <chrysolodivine@gmail.com>
2026-01-30 04:04:30 +0100

    removed app.listen

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
+/* app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`)
-})
+}) */
 
 export default app
